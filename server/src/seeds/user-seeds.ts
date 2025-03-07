fda57cc6f0ae49d1950c10045aea700d5f6bfc3e
joshsdesk <joshsdesk@gmail.com>
2025-03-06 21:43:33 -0700

    errrr

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -4,7 +4,7 @@ export const seedUsers = async () => {
   await User.bulkCreate([
     { username: 'JollyGuru', password: 'password' },
     { username: 'SunnyScribe', password: 'password' },
-    { username: 'Josh', password: 'password' },
+    { username: 'Josh', password: '12345' },
     { username: 'RadiantComet', password: 'password' },
     { username: 'Test', password: 'password' },
   ], { individualHooks: true });
